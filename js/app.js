537de93bf4334690490f20e078a8c7bd373e67f7
Natalie Chin <natalie.j.chin@gmail.com>
2015-09-19 21:40:29 -0500

    enemy-bug appears at random speed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    //set initial location and speed
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // a helper we've provided to easily load images ?
     this.sprite = 'images/enemy-bug.png';
-};
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +18,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    
+    //resets bugs position when hit border
+    if (this.x > 600) {
+            this.x = -50; }
+    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +34,45 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    
+    // load player image
+    this.sprite = 'images/char-boy.png';
+    
+    //set player initial location
+    this.x = 200;
+    this.y = 400;    
+    
+};
+
+//want the player to always start at the same spot
+Player.prototype.update = function(dt) {
+
+};
+
+//draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//moves the player
+Player.prototype.handleInput = function(dt) {
+  
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+
+// Instantiate all 3 enemies with random speed - one enemy for each row
+for (var i = 0; i < 3; i++) {
+    var randomSpeed = Math.floor(Math.random() * 5 + Math.random() * 5) * 100;
+    allEnemies.push(new Enemy(-50, (85 * i) + 60, randomSpeed));
+}
+
 // Place the player object in a variable called player
+var player = new Player();;
 
 
 
