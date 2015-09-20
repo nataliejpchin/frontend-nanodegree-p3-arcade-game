aba1458394dc5eab2649d76deb4e24167b532f83
Natalie Chin <natalie.j.chin@gmail.com>
2015-09-19 23:48:38 -0500

    remove error and clean code up

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    //set initial location and speed
+    // set initial location and speed
     this.x = x;
     this.y = y;
     this.speed = speed;
 
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images ?
+    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -19,16 +19,16 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x + this.speed * dt;
-    
-    //resets bugs position when hit border
+
+    // reset bugs position when hit border
     if (this.x > 600) {
-            this.x = -50; }
-            
-    //reset player position when collide with enemy-bug
+        this.x = -50;
+    }
+
+    // reset player position when collide with enemy-bug
     if (player.x > (this.x - 50) && player.x < (this.x + 50) && player.y > (this.y - 50) && player.y < (this.y + 50)) {
         player.resetPlayer();
     }
-
 };
 
 // Draw the enemy on the screen, required method for game
@@ -44,48 +44,44 @@ var initialPlayerX = 200;
 var initialPlayerY = 404;
 
 var Player = function() {
-  
-    //set player initial location
+    // set player initial location
     this.x = initialPlayerX;
-    this.y = initialPlayerY; 
-    
+    this.y = initialPlayerY;
+
     // load player image
     this.sprite = 'images/char-boy.png';
-    
 };
 
-//left empty because want the player to always start at the same spot
-Player.prototype.update = function(dt) {
-    if (this.y <= 0){
-      alert("You won!");
-      this.resetPlayer();
+// alert when player reach water and reset player position
+Player.prototype.update = function() {
+    if (this.y <= 0) {
+        alert("You won!");
+        this.resetPlayer();
     }
 };
 
-//draw the player on the screen
+// draw the player on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-//moves the player up, down, left, right
+// moves the player up, down, left, right
 Player.prototype.handleInput = function(keyUp) {
-    
-    if (keyUp === 'left' && this.x > 0){
-      this.x = this.x - 101;
-    } else if (keyUp === 'right' && this.x < 400){
+    if (keyUp === 'left' && this.x > 0) {
+        this.x = this.x - 101;
+    } else if (keyUp === 'right' && this.x < 400) {
         this.x = this.x + 101;
-    } else if (keyUp === 'up' && this.y > 0){
+    } else if (keyUp === 'up' && this.y > 0) {
         this.y = this.y - 83;
-    } else if (keyUp === 'down' && this.y < 404){
+    } else if (keyUp === 'down' && this.y < 404) {
         this.y = this.y + 83;
     }
-    
 };
 
 Player.prototype.resetPlayer = function() {
     this.x = initialPlayerX;
     this.y = initialPlayerY;
-}
+};
 
 
 // Now instantiate your objects.
@@ -99,7 +95,7 @@ for (var i = 0; i < 3; i++) {
 }
 
 // Place the player object in a variable called player
-var player = new Player();;
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
